a44bf180526d635a89cd9ebab32f5de8176653bb
ARAVIND-KUMAR-POTHULA <pothulaaravind241998@gmail.com>
2021-01-21 10:04:10 +0530

    ADDED.fumctionality of calculating employee wage including part time wage

--- empWageBuilder.java
+++ empWageBuilder.java
@@ -2,22 +2,27 @@ public class empWageBuilder {
     public static void main(String[] args) {
 
            // Constants
-        int IS_PRESENT = 1;
+        int IS_FULL_TIME = 1;
+	int IS_PART_TIME = 2;
 	int EMP_WAGE_PER_HOUR = 20;
 	//variables
 	int empHrs=0,empWage=0;
         // Computation
-        double empCheck = Math.floor(Math.random() * 10) % 2;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
         System.out.println(empCheck);
         // Case Checking
-        if (empCheck == IS_PRESENT) {
-            System.out.println("Employee is Present");
-		empHrs=8;
+        if (empCheck == IS_FULL_TIME) {
+            System.out.println(" Full Time Employee");
+		empHrs=12;
         }
-        else {
-	    System.out.println("Employee is Absent");
+        else if (empCheck == IS_PART_TIME){
+	    System.out.println("Part Time Employee");
+                empHrs=8;
+        }
+	else {
+		System.out.println("Employee is absent");
                 empHrs=0;
-        }
+	}
 	empWage = EMP_WAGE_PER_HOUR * empHrs;
 	System.out.println("employee wage = " + empWage);
     }
