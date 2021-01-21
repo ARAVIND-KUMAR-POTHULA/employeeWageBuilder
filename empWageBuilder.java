9fd0a2abf41dce808d0c9538726a7e3a6e9f47e7
ARAVIND-KUMAR-POTHULA <pothulaaravind241998@gmail.com>
2021-01-21 18:53:12 +0530

    ADDED.functionality of calculating total wage for multiple companies

--- empWageBuilder.java
+++ empWageBuilder.java
@@ -1,11 +1,8 @@
 public class empWageBuilder {
 	public static final int IS_FULL_TIME = 1;
         public static final int IS_PART_TIME = 2;
-        public static final int EMP_WAGE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_HRS_IN_MONTH = 100;
 
-	public static int computeEmpWage() {
+	public static int computeEmpWage(String company, int EMP_WAGE_PER_HOUR, int NUM_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH) {
 		 //variables
         	int empHrs = 0;
         	int empWage = 0;
@@ -40,6 +37,7 @@ public class empWageBuilder {
 
 
        public static void main(String[] args) {
-	computeEmpWage();
+	computeEmpWage("Dmart", 20, 2, 10);
+	computeEmpWage("Reliance", 10, 4, 20);
        }
 }
