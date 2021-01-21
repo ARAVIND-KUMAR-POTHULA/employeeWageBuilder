dfffb6db488d4bd9ca53491b568deef7d11b7ded
ARAVIND-KUMAR-POTHULA <pothulaaravind241998@gmail.com>
2021-01-21 19:31:49 +0530

    ADDED.functionality of calculating and save total wage for each company

--- empWageBuilder.java
+++ empWageBuilder.java
@@ -2,11 +2,23 @@ public class empWageBuilder {
 	public static final int IS_FULL_TIME = 1;
         public static final int IS_PART_TIME = 2;
 
-	public static int computeEmpWage(String company, int EMP_WAGE_PER_HOUR, int NUM_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH) {
+	 private final String company;
+	 private final int EMP_WAGE_PER_HOUR;
+	 private final int NUM_OF_WORKING_DAYS;
+	 private final int MAX_HRS_IN_MONTH;
+	 private int totalEmpWage;
+
+	public empWageBuilder(String company, int EMP_WAGE_PER_HOUR, int NUM_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH) {
+		this.company = company;
+		this.EMP_WAGE_PER_HOUR =  EMP_WAGE_PER_HOUR;
+		this.NUM_OF_WORKING_DAYS = NUM_OF_WORKING_DAYS;
+		this.MAX_HRS_IN_MONTH = MAX_HRS_IN_MONTH;
+
+	}
+	public void computeEmpWage() {
 		 //variables
         	int empHrs = 0;
         	int empWage = 0;
-        	int totalEmpWage = 0;
         	int totalWorkingDays = 0;
         	int totalEmpHrs = 0;
         	// Computation
@@ -31,13 +43,19 @@ public class empWageBuilder {
         		System.out.println("Day: " + totalWorkingDays + "  Emp Hr: " + empHrs);
         	}
         	totalEmpWage = EMP_WAGE_PER_HOUR * totalEmpHrs;
-        	System.out.println("total employee wage = " + totalEmpWage);
-		return totalEmpWage;
 	}
 
+	@Override
+       public String toString() {
+		return "total emp wage for company: " +company+ " is: " +totalEmpWage;
+       }
 
        public static void main(String[] args) {
-	computeEmpWage("Dmart", 20, 2, 10);
-	computeEmpWage("Reliance", 10, 4, 20);
+	empWageBuilder dmart = new empWageBuilder("Dmart", 20, 2, 10);
+	empWageBuilder reliance = new empWageBuilder("Reliance", 10, 4, 20);
+	dmart.computeEmpWage();
+	System.out.println(dmart);
+	reliance.computeEmpWage();
+        System.out.println(reliance);
        }
 }
