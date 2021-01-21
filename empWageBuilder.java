a280337cadc809e565a1a5dbe79bef45ca314908
ARAVIND-KUMAR-POTHULA <pothulaaravind241998@gmail.com>
2021-01-21 09:36:24 +0530

    ADDED.fumctionality of checking employee is present or absent

--- empWageBuilder.java
+++ empWageBuilder.java
@@ -1,7 +1,18 @@
 public class empWageBuilder {
     public static void main(String[] args) {
 
-        // Welcome message
-        System.out.println("Welcome to Employee Wage Builder");
+           // Contains
+        int IS_PRESENT = 1;
+
+        // Computation
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        System.out.println(empCheck);
+        // Case Checking
+        if (empCheck == IS_PRESENT) {
+            System.out.println("Employee is Present");
+        }
+        else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
