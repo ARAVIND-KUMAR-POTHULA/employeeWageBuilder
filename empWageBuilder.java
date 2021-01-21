bb7f9c88bcd38080976ec90d9d4408489b658f13
ARAVIND-KUMAR-POTHULA <pothulaaravind241998@gmail.com>
2021-01-21 10:17:41 +0530

    ADDED.fumctionality of calculating employee wages using switch case

--- empWageBuilder.java
+++ empWageBuilder.java
@@ -1,27 +1,29 @@
 public class empWageBuilder {
+	public static final int IS_FULL_TIME = 1;
+        public static final int IS_PART_TIME = 2;
+        public static final int EMP_WAGE_PER_HOUR = 20;
+
     public static void main(String[] args) {
 
-           // Constants
-        int IS_FULL_TIME = 1;
-	int IS_PART_TIME = 2;
-	int EMP_WAGE_PER_HOUR = 20;
 	//variables
 	int empHrs=0,empWage=0;
         // Computation
-        double empCheck = Math.floor(Math.random() * 10) % 3;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
         System.out.println(empCheck);
         // Case Checking
-        if (empCheck == IS_FULL_TIME) {
-            System.out.println(" Full Time Employee");
-		empHrs=12;
-        }
-        else if (empCheck == IS_PART_TIME){
-	    System.out.println("Part Time Employee");
-                empHrs=8;
-        }
-	else {
-		System.out.println("Employee is absent");
-                empHrs=0;
+        switch (empCheck) {
+
+		case IS_FULL_TIME:
+            		System.out.println(" Full Time Employee");
+			empHrs=12;
+			break;
+        	case IS_PART_TIME:
+	                System.out.println("Part Time Employee");
+                        empHrs=8;
+			 break;
+		default:
+			System.out.println("Employee is absent");
+                	empHrs=0;
 	}
 	empWage = EMP_WAGE_PER_HOUR * empHrs;
 	System.out.println("employee wage = " + empWage);
