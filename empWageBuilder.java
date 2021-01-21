a8903afba344e0212962ca1feb7953d629b19d22
ARAVIND-KUMAR-POTHULA <pothulaaravind241998@gmail.com>
2021-01-21 09:50:50 +0530

    ADDED.fumctionality of calculating employee wage

--- empWageBuilder.java
+++ empWageBuilder.java
@@ -1,18 +1,24 @@
 public class empWageBuilder {
     public static void main(String[] args) {
 
-           // Contains
+           // Constants
         int IS_PRESENT = 1;
-
+	int EMP_WAGE_PER_HOUR = 20;
+	//variables
+	int empHrs=0,empWage=0;
         // Computation
         double empCheck = Math.floor(Math.random() * 10) % 2;
         System.out.println(empCheck);
         // Case Checking
         if (empCheck == IS_PRESENT) {
             System.out.println("Employee is Present");
+		empHrs=8;
         }
         else {
-            System.out.println("Employee is Absent");
+	    System.out.println("Employee is Absent");
+                empHrs=0;
         }
+	empWage = EMP_WAGE_PER_HOUR * empHrs;
+	System.out.println("employee wage = " + empWage);
     }
 }
